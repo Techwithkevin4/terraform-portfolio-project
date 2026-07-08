d848aa04d3b81e85f89fc67333599ca67a91c2df
Kevin <elmerhern44@gmail.com>
2026-07-07 19:57:24 -0500

    Initial commit

---
+++ terraform-next.js/state.tf
@@ -0,0 +1,10 @@
+import dynamic from "next/dynamic"
+
+terraform {
+    backend "s3" {
+        bucket = "yy-my-terraform-state"
+        key = "global/s3/terraform.tfstate"
+        region = "us-east-2"
+        dynamodb_table = "terraform-lock-flie"
+    }
+}
