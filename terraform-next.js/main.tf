2f04b3e1ba0c6249d177db255b2f9a5971b33a69
Techwithkevin4 <elmerhern44@gmail.com>
2026-07-08 22:18:54 -0500

    Update main.tf

--- terraform-next.js/main.tf
+++ terraform-next.js/main.tf
@@ -1,112 +1,116 @@
 provider "aws" {
-    region = "us-east-2"
+  region = "us-west-2"
 }
 
 # S3 Bucket
 resource "aws_s3_bucket" "nextjs_bucket" {
-    bucket = "nextjs_portfolio-bucket-yy"
+  bucket = "nextjs-portfolio-yy-12345"
 }
 
-# Ownership Control
+# Ownership Control (disable ACLs)
 resource "aws_s3_bucket_ownership_controls" "nextjs_bucket_ownership_controls" {
-    bucket = aws_s3_bucket.nextjs_bucket.id
+  bucket = aws_s3_bucket.nextjs_bucket.id
 
-    rule {
-        object_ownership = "BucketOwnerPreferred 
-    }
+  rule {
+    object_ownership = "BucketOwnerEnforced"
+  }
 }
 
-# Block public Access
+# Block Public Access
 resource "aws_s3_bucket_public_access_block" "nextjs_bucket_public_access_block" {
-    bucket = aws_s3_bucket.nextjs_bucket.id
+  bucket = aws_s3_bucket.nextjs_bucket.id
 
-    block_public_acls = false
-    block_public_policy = false
-    ignore_public_acls = false
-    restrict_public_buckets = false
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
-# Bucket ACL
-resource "aws_s3_bucket_acl" "nextjs_bucket_acl" {
-
-    depends_on = [
-        aws_s3_bucket_ownership_controls.nextjs,
-        aws_s3_bucket_public_access_block.nextjs_bucket_public_access_block
-    
-    ]
-    
-    bucket = aws_s3_bucket.nextjs_bucket.id
-    acl = "public-read"
-}
-# bucket policy
-
-resource "aws_s3_bucket_policy" "nextjs_bucket_policy" {
-    bucket = aws_s3_bucket.nextjs_bucket.id
-
-    policy = jasoncode(({
-        version = "2012-10-17"
-        Statement = [
-            {
-                Sid = "PublicReadGetObject"
-                Effect = "Allow"
-                Principal = "*"
-                Action = "s3:GetObject"
-                Resource = "$[aws_s3_bucket.nextjs_bucket.arn]/*"
-            }
-        ]  
-    }))
-}
-
-# Origin Access Identity
+# CloudFront Origin Access Identity
 resource "aws_cloudfront_origin_access_identity" "origin_access_identity" {
-    comment = "OAI for Next.JS portfolio site"
+  comment = "OAI for Next.JS portfolio site"
 }
 
-# Cloudfront distribution
+# Allow CloudFront to read from S3
+resource "aws_s3_bucket_policy" "nextjs_bucket_policy" {
+  bucket = aws_s3_bucket.nextjs_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Sid    = "AllowCloudFrontRead"
+        Effect = "Allow"
+
+        Principal = {
+          AWS = aws_cloudfront_origin_access_identity.origin_access_identity.iam_arn
+        }
+
+        Action = [
+          "s3:GetObject"
+        ]
+
+        Resource = "${aws_s3_bucket.nextjs_bucket.arn}/*"
+      }
+    ]
+  })
+}
+
+# CloudFront Distribution
 resource "aws_cloudfront_distribution" "nextjs_distribution" {
 
-    origin {
-        domain_name = aws_s3_bucket.nextjs_bucket.bucket_regional_domain_name,
-        origin_id = "S3-nextjs-portfolio-bucket"
+  depends_on = [
+    aws_s3_bucket_policy.nextjs_bucket_policy
+  ]
 
-        s3_origin_config {
-            origin_access_identity = aws_cloudfront_origin_access_identity.origin_access_identity.cloudfront_access_identity_path
-        }
+  enabled = true
+
+  default_root_object = "index.html"
+
+  origin {
+    domain_name = aws_s3_bucket.nextjs_bucket.bucket_regional_domain_name
+    origin_id   = "S3-nextjs-origin"
+
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.origin_access_identity.cloudfront_access_identity_path
     }
+  }
 
-    enabled = true
-    is_ipv6 = true
-    comment = "Next.js portfolio site"
-    default_root_object = "index.html"
+  default_cache_behavior {
 
-    default_cache_behavior {
-        allow_methods = ["GET","HEAD","OPTIONS"]
-        cache_methods = ["GET","HEAD"]
-        target_origin_id = "S3-nextjs-portfolio-bucket"
+    target_origin_id = "S3-nextjs-origin"
 
-        forwarded_values {
-            query_string = false
-            cookies {
-                forward = "none"
+    allowed_methods = [
+      "GET",
+      "HEAD",
+      "OPTIONS"
+    ]
 
-            }
-        }
-        viewer_protocal_policy = "redirect-to-https"
-        min_ttl = 0
-        default_ttl = 3600
-        max_ttl = 86400
+    cached_methods = [
+      "GET",
+      "HEAD"
+    ]
+
+    viewer_protocol_policy = "redirect-to-https"
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
     }
+  }
 
-     restrictions {
-        geo_restriction {
-            restriction_type = none
-        }
+  restrictions {
+
+    geo_restriction {
+      restriction_type = "none"
     }
+  }
 
-    viewer_certificate {
-
-        cloudfront_default_certificate = true
-        
-    }
-
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
 }
